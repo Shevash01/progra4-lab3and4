23ba8037eb7bfe3847d9ad0f55a9f07563bd739b
Shevash01 <sebastian.campos.calvo@est.una.ac.cr>
2025-04-02 11:20:15 -0600

    First commit Lab3

---
+++ src/Components/AnonymousUserComponent.jsx
@@ -0,0 +1,9 @@
+
+export function AnonymousUserComponent(){
+
+    return (
+        <>
+        <h2>Hola, por favor acceder al sitio para obtener mayor informacion</h2>
+        </>
+    )
+}
---
+++ src/Components/WelcomeWidget.jsx
@@ -0,0 +1,9 @@
+
+export default function WelcomeWidget({email}){
+
+    return(
+        <>
+        <h2>WELCOME TO OUR SITE {email}</h2>
+        </>
+    )
+}
